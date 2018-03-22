6b3eb20ee22142a109aecdf2f8657b496e3991d1
jhoneagle <eaglekjohn@gmail.com>
2018-03-22 13:13:00 +0200

    sqlacademy included

--- application/__init__.py
+++ application/__init__.py
@@ -3,4 +3,14 @@
 from flask import Flask
 app = Flask(__name__)
 
+from flask_sqlalchemy import SQLAlchemy
+app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///webdata.db"
+app.config["SQLALCHEMY_ECHO"] = True
+
+db = SQLAlchemy(app)
+
 import application.control
+
+from application.webdata import models
+
+db.create_all()
